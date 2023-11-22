f3a2c795f1d05a81627b93b7f123e914f846f0b5
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-11-22 02:47:33 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,320 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Flex,
+  Layout,
+  Menu,
+  theme,
+  Typography,
+  Input,
+  Card,
+  List,
+  Divider,
+  Tag,
+  Button,
+  Modal,
+} from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
+import { subjects } from './data/subjects';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
+const { Search } = Input;
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const [role, setRole] = useState('');
+  const [showSubjects, setShowSubjects] = useState(false);
+  const [output, setOutput] = useState(['Please enter your name.']);
+  const [inputValue, setInputValue] = useState('');
+  const [nextIndex, setNextIndex] = useState(0);
+  const [selectedSubjects, setSelectedSubjects] = useState([]);
+  const [openModel, setOpenModel] = useState(false);
+
+  const welcome = (value) => {
+    setNextIndex(1);
+    return `Hello  ${value}. Welcome to Edushare, are you a student or a teacher?`;
+  };
+
+  const areYouContentCreator = (value) => {
+    switch (value.toLowerCase()) {
+      case 'student':
+        setNextIndex(2);
+        setRole('student');
+        return 'Hello Student. What is your favourite subject?';
+      case 'teacher':
+        setNextIndex(2);
+        setRole('teacher');
+        return 'Hello Teacher. What subject do you wish to teach?';
+      default:
+        return 'I do not recognise your input, please use student or teacher';
+    }
+  };
+
+  const whatSubjectAreYouInterestedIn = (value) => {
+    setNextIndex(3);
+    switch (role) {
+      case 'student':
+        setShowSubjects(true);
+        return `Excellent choice! I like ${value} as well. Now can you please select the 3 subjects you are most interested in.`;
+      case 'teacher':
+        return `Excellent choice! I like ${value} as well. You can now continue onto the home page as your setup is complete.`;
+      default:
+        break;
+    }
+  };
+
+  const subjectOptions = () => {
+    setNextIndex(4);
+    return 'subject stuff';
+  };
+
+  const listOfFn = [
+    welcome,
+    areYouContentCreator,
+    whatSubjectAreYouInterestedIn,
+    subjectOptions,
+  ];
+
+  const handleSubmission = (value) => {
+    const chatFn = listOfFn[nextIndex];
+    const question = chatFn(value);
+    setOutput((prev) => [...prev, value]);
+
+    setOutput((prev) => [...prev, question]);
+    setInputValue('');
+  };
+
+  const handleSelectSubject = (item) => {
+    if (selectedSubjects.length < 3) {
+      setSelectedSubjects((prev) => [...new Set([...prev, item])]);
+    }
+  };
+
+  const handleClose = (removedTag) => {
+    const newTags = selectedSubjects.filter((tag) => tag !== removedTag);
+    setSelectedSubjects(newTags);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Layout className='layout' style={{ height: '100dvh' }}>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Title style={{ color: 'white', paddingRight: '1rem', margin: 0 }}>
+          Edushare
+        </Title>
+        <div className='demo-logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['2']}
+          items={new Array(6).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content
+        style={{
+          display: 'flex',
+          padding: '0px',
+        }}
+      >
+        <div
+          className='site-layout-content'
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            background: colorBgContainer,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Title>Getting Started...</Title>
+          <Flex vertical className='chat-box'>
+            <ul className='text-entry'>
+              {output.map((entry, index) => {
+                return (
+                  <li key={index} className='chat-box-item'>
+                    {entry}
+                  </li>
+                );
+              })}
+            </ul>
+            {showSubjects && (
+              <div className='list-container'>
+                <List
+                  style={{ display: 'flex' }}
+                  grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 2,
+                    lg: 2,
+                    xl: 2,
+                    xxl: 2,
+                  }}
+                  dataSource={subjects}
+                  renderItem={(item, index) => (
+                    <List.Item key={index} className='list-item'>
+                      <Card
+                        style={{ height: '450px', fontSize: '1rem' }}
+                        hoverable
+                        cover={
+                          <img
+                            alt={item.name}
+                            style={{ borderRadius: 0, height: '200px' }}
+                            src={`/assets/${item.image_name}`}
+                          />
+                        }
+                        title={
+                          <Title level={2} style={{ paddingTop: '1rem' }}>
+                            {item.name}
+                          </Title>
+                        }
+                        onClick={() => {
+                          handleSelectSubject(item);
+                        }}
+                      >
+                        <span style={{ fontWeight: 500, fontSize: '1.5rem' }}>
+                          {item.level}
+                        </span>
+                        <Divider style={{ margin: '8px' }} />
+                        {item.description}
+                      </Card>
+                    </List.Item>
+                  )}
+                />
+              </div>
+            )}
 
-export default App
+            {selectedSubjects.length > 0 && (
+              <>
+                <span
+                  style={{ fontSize: '2rem', textAlign: 'end' }}
+                >{`${selectedSubjects.length} out of 3`}</span>
+                <div>
+                  {selectedSubjects.map((subject) => {
+                    return (
+                      <Tag
+                        key={subject}
+                        style={{
+                          userSelect: 'none',
+                          width: 'fit-content',
+                          padding: '0.5rem',
+                          fontSize: '1rem',
+                          marginTop: '2rem',
+                          cursor: 'pointer',
+                        }}
+                        onClick={() => handleClose(subject)}
+                      >
+                        {`${subject.name} (${subject.level})`}
+                      </Tag>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+            {selectedSubjects.length === 3 && (
+              <Flex
+                gap={'1rem'}
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'start',
+                  paddingTop: '1rem',
+                }}
+              >
+                <span
+                  style={{
+                    fontSize: '1rem',
+
+                    textAlign: 'center',
+                  }}
+                >
+                  Are you happy with your choices?
+                </span>
+
+                <Button
+                  type='default'
+                  onClick={() => {
+                    setSelectedSubjects([]);
+                  }}
+                >
+                  {' '}
+                  No{' '}
+                </Button>
+                <Button
+                  type='primary'
+                  onClick={() => {
+                    setOpenModel(true);
+                  }}
+                >
+                  {' '}
+                  Yes{' '}
+                </Button>
+              </Flex>
+            )}
+            <Search
+              placeholder='input your response here'
+              loading={false}
+              value={inputValue}
+              enterButton='Enter'
+              onPressEnter={(value) => handleSubmission(value.target.value)}
+              onChange={(e) => setInputValue(e.target.value)}
+              style={{ paddingTop: '2rem' }}
+            />
+          </Flex>
+        </div>
+        {openModel && (
+          <Modal
+            title='You are almost Done...'
+            style={{
+              top: 20,
+              fontSize: '1rem',
+            }}
+            open={openModel}
+            onOk={() => {
+              setOpenModel(false);
+              window.location.reload();
+            }}
+            onCancel={() => {
+              setSelectedSubjects([]);
+              setOpenModel(false);
+            }}
+          >
+            <p style={{ fontSize: '1rem' }}>
+              Check the summary below and press OK to set your homepage.
+            </p>
+            {selectedSubjects.map((item) => {
+              return (
+                <p
+                  key={item}
+                  style={{ fontSize: '1rem', paddingTop: '0.5rem' }}
+                >
+                  {item.name} {item.level}
+                </p>
+              );
+            })}
+          </Modal>
+        )}
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+export default App;
---
+++ src/data/subjects.js
@@ -0,0 +1,196 @@
+export const subjects = [
+  {
+    name: 'Mathematics',
+    level: 'Basic',
+    description:
+      'Fundamental concepts in arithmetic, algebra, geometry, and calculus.',
+    image_name: 'maths.jpg',
+  },
+  {
+    name: 'Science',
+    level: 'Basic',
+    description: 'Covers biology, chemistry, physics, and earth sciences.',
+    image_name: 'science.jpg',
+  },
+  {
+    name: 'History',
+    level: 'Basic',
+    description: 'Study of past events, societies, and civilizations.',
+    image_name: 'history.jpg',
+  },
+  {
+    name: 'Literature',
+    level: 'Basic',
+    description:
+      'Exploration of various written works, authors, and literary forms.',
+    image_name: 'literature.jpg',
+  },
+  {
+    name: 'Computer Science',
+    level: 'Basic',
+    description:
+      'Getting started with Computer Science. Into to algorithms and programming.',
+    image_name: 'computer-science.jpg',
+  },
+  {
+    name: 'Computer Science',
+    level: 'Intermediate',
+    description:
+      'Deals with algorithms, programming, and computational thinking.',
+    image_name: 'computer-science.jpg',
+  },
+  {
+    name: 'English Languages',
+    level: 'Basic',
+    description: "Learning languages other than one's native language.",
+    image_name: 'english.jpg',
+  },
+  {
+    name: 'English Languages',
+    level: 'Intermediate',
+    description: "Learning languages other than one's native language.",
+    image_name: 'english.jpg',
+  },
+  {
+    name: 'English Languages',
+    level: 'Advanced',
+    description: "Learning languages other than one's native language.",
+    image_name: 'english.jpg',
+  },
+
+  {
+    name: 'French Languages',
+    level: 'Basic',
+    description: "Learning languages other than one's native language.",
+    image_name: 'french.jpg',
+  },
+  {
+    name: 'French Languages',
+    level: 'Intermediate',
+    description: "Learning languages other than one's native language.",
+    image_name: 'french.jpg',
+  },
+  {
+    name: 'French Languages',
+    level: 'Advanced',
+    description: "Learning languages other than one's native language.",
+    image_name: 'french.jpg',
+  },
+  {
+    name: 'German Languages',
+    level: 'Basic',
+    description: "Learning languages other than one's native language.",
+    image_name: 'german.jpg',
+  },
+  {
+    name: 'German Languages',
+    level: 'Intermediate',
+    description: "Learning languages other than one's native language.",
+    image_name: 'german.jpg',
+  },
+  {
+    name: 'German Languages',
+    level: 'Advanced',
+    description: "Learning languages other than one's native language.",
+    image_name: 'german.jpg',
+  },
+  {
+    name: 'Chinese Languages',
+    level: 'Basic',
+    description: "Learning languages other than one's native language.",
+    image_name: 'chinese.jpg',
+  },
+  {
+    name: 'Chinese Languages',
+    level: 'Intermediate',
+    description: "Learning languages other than one's native language.",
+    image_name: 'chinese.jpg',
+  },
+  {
+    name: 'Chinese Languages',
+    level: 'Advanced',
+    description: "Learning languages other than one's native language.",
+    image_name: 'chinese.jpg',
+  },
+  {
+    name: 'Spanish Languages',
+    level: 'Basic',
+    description: "Learning languages other than one's native language.",
+    image_name: 'spanish.jpg',
+  },
+  {
+    name: 'Spanish Languages',
+    level: 'Intermediate',
+    description: "Learning languages other than one's native language.",
+    image_name: 'spanish.jpg',
+  },
+  {
+    name: 'Spanish Languages',
+    level: 'Advanced',
+    description: "Learning languages other than one's native language.",
+    image_name: 'spanish.jpg',
+  },
+  {
+    name: 'Psychology',
+    level: 'Intermediate',
+    description: 'Study of the human mind, behavior, and mental processes.',
+    image_name: 'psychology.jpg',
+  },
+  {
+    name: 'Philosophy',
+    level: 'Advanced',
+    description:
+      'Inquiry into fundamental questions about existence, knowledge, ethics, and more.',
+    image_name: 'philosophy.jpg',
+  },
+  {
+    name: 'Sociology',
+    level: 'Advanced',
+    description:
+      'Examination of human societies, social behavior, and structures.',
+    image_name: 'sociology.jpg',
+  },
+  {
+    name: 'Economics',
+    level: 'Advanced',
+    description:
+      'Analysis of production, distribution, and consumption of goods and services.',
+    image_name: 'economics.jpg',
+  },
+  {
+    name: 'Cooking',
+    level: 'Basics',
+    description: 'Intro in how to cook at home',
+    image_name: 'cooking.jpg',
+  },
+  {
+    name: 'Cooking',
+    level: 'Intermediate',
+    description: 'Teaching more Intermediate cooking skills',
+    image_name: 'cooking.jpg',
+  },
+  {
+    name: 'Cooking',
+    level: 'Advanced',
+    description: 'Advanced skill in how to cook at home and in a restaurant',
+    image_name: 'cooking.jpg',
+  },
+  {
+    name: 'Engine Repair',
+    level: 'Basics',
+    description: 'Intro in how to repair a combustion engine',
+    image_name: 'engine-repair.jpg',
+  },
+  {
+    name: 'Engine Repair',
+    level: 'Intermediate',
+    description: 'How to repair a car engine for Intermediates',
+    image_name: 'engine-repair.jpg',
+  },
+  {
+    name: 'Engine Repair',
+    level: 'Advanced',
+    description: 'Advanced techniques in repairing car and truck engines',
+    image_name: 'engine-repair.jpg',
+  },
+];
